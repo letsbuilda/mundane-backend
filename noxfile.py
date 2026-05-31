eca7ab0b2edaeb9d20ad13f1c618d224f510c73a
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2026-05-31 15:36:44 +0000

    Appease again
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

--- noxfile.py
+++ noxfile.py
@@ -36,7 +36,7 @@ def lints(session: nox.Session) -> None:
     session.run("prek", "run", "--all-files")
     session.run("ruff", "format", ".")
     session.run("ruff", "check", "--fix", ".")
-    session.run("mypy", "--strict", "src/", "packages/", "tests/")
+    session.run("mypy", "--strict", "src/", "tests/")
     session.run("ty", "check", ".")
 
 
