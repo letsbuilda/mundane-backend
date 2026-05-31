79eab37440521a1b5a7daa82077ce36e27a402b8
Bradley Reynolds <bradley.reynolds@tailstory.dev>
2026-05-31 15:29:21 +0000

    Modernize
    Signed-off-by: Bradley Reynolds <bradley.reynolds@tailstory.dev>

--- noxfile.py
+++ noxfile.py
@@ -33,10 +33,11 @@ def tests(session: nox.Session) -> None:
 @nox.session
 def lints(session: nox.Session) -> None:
     """Run lints."""
-    session.run("pre-commit", "run", "--all-files")
+    session.run("prek", "run", "--all-files")
     session.run("ruff", "format", ".")
     session.run("ruff", "check", "--fix", ".")
-    session.run("mypy", "--strict", "src/")
+    session.run("mypy", "--strict", "src/", "packages/", "tests/")
+    session.run("ty", "check", ".")
 
 
 @nox.session
